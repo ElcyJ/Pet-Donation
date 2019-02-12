f5b7983320e1fe0af78ad2f43bcea3069983929c
ElcyJ <cyzinha508@gmail.com>
2019-02-12 13:33:24 -0200

    chat implementation

--- app/src/main/java/com/example/elcy/apppet/Chat/ChatActivity.java
+++ app/src/main/java/com/example/elcy/apppet/Chat/ChatActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 
 import com.example.elcy.apppet.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -49,7 +50,7 @@ public class ChatActivity extends AppCompatActivity {
 
         mRecyclerView = findViewById(R.id.recyclerViewChat);
         mRecyclerView.setNestedScrollingEnabled(false);
-        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setHasFixedSize(false);
         mChatLayoutManager = new LinearLayoutManager(ChatActivity.this);
         mRecyclerView.setLayoutManager(mChatLayoutManager);
         mChatAdapter = new ChatAdapter(ChatActivity.this, getDataSetChat());
@@ -89,7 +90,58 @@ public class ChatActivity extends AppCompatActivity {
                 if(dataSnapshot.exists()){
                     chatId = dataSnapshot.getValue().toString();
                     mDatabaseChat = mDatabaseChat.child(chatId);
+
+                    getChatMessages();
                 }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getChatMessages() {
+        mDatabaseChat.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if(dataSnapshot.exists()){
+                    String message = null;
+                    String createByUser = null;
+
+                    if(dataSnapshot.child("text").getValue() != null){
+                        message =dataSnapshot.child("text").getValue().toString();
+                    }
+                    if(dataSnapshot.child("createByUser").getValue() != null){
+                        createByUser =dataSnapshot.child("createByUser").getValue().toString();
+                    }
+                    if(message!=null && createByUser!=null){
+                        Boolean currentUserBoolean = false;
+                        if (createByUser.equals(currentUser)){
+                            currentUserBoolean = true;
+                        }
+                        ChatObject newMessage = new ChatObject(message, currentUserBoolean);
+                        resultsChat.add(newMessage);
+                        mChatAdapter.notifyDataSetChanged();
+                    }
+
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
             }
 
             @Override
--- app/src/main/java/com/example/elcy/apppet/Chat/ChatAdapter.java
+++ app/src/main/java/com/example/elcy/apppet/Chat/ChatAdapter.java
@@ -2,19 +2,22 @@ package com.example.elcy.apppet.Chat;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.elcy.apppet.R;
 
 import java.util.List;
 
-public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.FavoriteViewHolder>{
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder>{
     private Context context;
     private List<ChatObject> chatList;
 
@@ -23,11 +26,17 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.FavoriteViewHo
         this.chatList = favoriteList;
     }
 
-    public static class FavoriteViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public static class ChatViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-        public FavoriteViewHolder(View itemView) {
+        private TextView mMessage;
+        private LinearLayout mContainer;
+
+        public ChatViewHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
+
+            mMessage = itemView.findViewById(R.id.message);
+            mContainer = itemView.findViewById(R.id.container);
 
         }
 
@@ -39,17 +48,28 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.FavoriteViewHo
     }
 
     @Override
-    public FavoriteViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ChatViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate((R.layout.item_favorite), null, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate((R.layout.item_chat), null, false);
         RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         view.setLayoutParams(lp);
 
-        return new FavoriteViewHolder(view);
+        return new ChatViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(FavoriteViewHolder holder, int position) {
+    public void onBindViewHolder(ChatViewHolder holder, int position) {
+        holder.mMessage.setText(chatList.get(position).getMessage());
+        if(chatList.get(position).getCurrentUser()){
+            holder.mMessage.setGravity(Gravity.END);
+            holder.mMessage.setTextColor(Color.parseColor("#b596fd"));
+            holder.mContainer.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
+        else {
+            holder.mMessage.setGravity(Gravity.END);
+            holder.mMessage.setTextColor(Color.parseColor("#cabcfa"));
+            holder.mContainer.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
 
     }
 
